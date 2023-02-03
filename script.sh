544278aeb1b2923d1770815058094f1a80e51548
Pakas142 <prakashs142@gmail.com>
2023-02-03 18:08:29 +0530

    first repository

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ then
 	git push origin main
 else
 	git init
-	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
+	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	echo "welcome to guvi" |tee welcome{1..10}.txt
 	git add .
 	git commit -m "first repository"
