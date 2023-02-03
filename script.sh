b1d9a7121db3a04c915d3ca5aaf724b7a425739c
Pakas142 <prakashs142@gmail.com>
2023-02-03 17:27:37 +0530

    first repository

---
+++ script.sh
@@ -0,0 +1,20 @@
+read var1
+read var2
+if [ $var1 -eq $var2 ]
+then
+	git init
+	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
+	echo "welcome to guvi" | tee welcome{1..10}.txt
+	tar -cf welcome.tar *.*
+	git add .
+	git commit -m "first repository"
+	git push origin main
+else
+	git init
+	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
+	echo "welcome to guvi" |tee welcome{1..10}.txt
+	git add .
+	git commit -m "first repository"
+	git checkout -b develop master
+	git push origin develop
+fi
