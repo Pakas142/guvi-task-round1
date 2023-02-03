03eb104021ff5f08c9b20a15834724c64c5060c3
Pakas142 <prakashs142@gmail.com>
2023-02-03 17:57:09 +0530

    first repository

--- script.sh
+++ script.sh
@@ -3,19 +3,18 @@ read var2
 if [ $var1 -eq $var2 ]
 then
 	git init
-	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	echo "welcome to guvi" | tee welcome{1..10}.txt
 	tar -cf welcome.tar *.*
 	git add .
 	git commit -m "first repository"
-	git checkout -b main master
+	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	git push origin main
 else
 	git init
-	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	echo "welcome to guvi" |tee welcome{1..10}.txt
 	git add .
 	git commit -m "first repository"
 	git checkout -b develop master
+	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	git push origin develop
 fi
