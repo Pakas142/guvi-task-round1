6b8b7b85cc9b4988b978d49fd3093d7ed595a262
Pakas142 <prakashs142@gmail.com>
2023-02-03 18:05:58 +0530

    first repository

--- script.sh
+++ script.sh
@@ -14,7 +14,7 @@ else
 	echo "welcome to guvi" |tee welcome{1..10}.txt
 	git add .
 	git commit -m "first repository"
-	git checkout develop master
+	git checkout develop
 	git config --global remote.origin.url https://oauth2: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@github.com/Pakas142/guvi-task-round1.git
 	git push origin develop
 fi
